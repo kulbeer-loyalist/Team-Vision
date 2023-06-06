0a9c9267ba24182bffe8e4fc23cbc67e54cd0b30
roshikdahal <kumardahal536@gmail.com>
2023-06-06 00:18:15 -0400

    custom logger is written

--- src/ASLD_step2/__init__.py
+++ src/ASLD_step2/__init__.py
@@ -1,18 +1,55 @@
+"""
+author @ kumar dahal
+date: jun 5 2023
+this is the logger of the project
+"""
+
 import os
 import sys
 import logging
+import pandas as pd
+from ASLD_step2.constants import get_current_time_stamp
 
-logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
-log_dir = "logs"
-log_filepath = os.path.join(log_dir, "running_logs.log")
-os.makedirs(log_dir, exist_ok=True)
 
-logging.basicConfig(
-    level=logging.INFO, 
-    format=logging_str,
-    handlers=[
-        logging.FileHandler(log_filepath),
-        # logging.StreamHandler(sys.stdout),
-    ])
+LOG_DIR="logs"
 
-logger = logging.getLogger("ASLDLogger")
+"""
+this function is written to create the log file ,
+where we fetch the constant current time stamp function and create the file so that at each run we can create unique file
+"""
+def get_log_file_name():
+    return f"log_{(get_current_time_stamp)}.log"
+
+LOG_FILE_NAME = get_log_file_name()
+#check if exist or not if exist don't create it
+os.makedirs(LOG_DIR,exist_ok=True)
+#logs/1685974218.log
+LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
+
+
+
+logging.basicConfig(filename=LOG_FILE_PATH,
+filemode="w",
+format='[%(asctime)s]^;%(levelname)s^;%(lineno)d^;%(filename)s^;%(funcName)s()^;%(message)s',
+level=logging.INFO
+)
+
+"""
+this function is written to get the error in particular file line number function name and with  message
+"""
+
+def get_log_dataframe(file_path):
+    data=[]
+    with open(file_path) as log_file:
+        for line in log_file.readlines():
+            data.append(line.split("^;"))
+
+    log_df = pd.DataFrame(data)
+    columns=["Time stamp","Log Level","line number","file name","function name","message"]
+    log_df.columns=columns
+    
+    log_df["log_message"] = log_df['Time stamp'].astype(str) +":$"+ log_df["message"]
+
+    return log_df[["log_message"]]
+
+
