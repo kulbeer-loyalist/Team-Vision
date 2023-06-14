b75f44fd0a702bc8b239c9db9d67c04887bf7092
Ajit Gurung <ajitgurung04@gmail.com>
2023-06-14 07:46:04 -0400

    train_test_split v1

--- src/ASLD_step2/components/data_ingestion.py
+++ src/ASLD_step2/components/data_ingestion.py
@@ -5,14 +5,14 @@ this class is created to retrive the data ,extract the data ,and split the data 
 since we have assigned properties to entity we can use those entity here to perform all those task
 """
 
-from ASLD_step2.entity import DataIngestionConfig
-from ASLD_step2.logger import logging
-from ASLD_step2.Exception import ASLDException
-import sys
-import os
-import urllib.request as request
-from pathlib import Path
-from ASLD_step2.utils import get_size
+# from ASLD_step2.entity import DataIngestionConfig
+# from ASLD_step2.logger import logging
+# from ASLD_step2.Exception import ASLDException
+# import sys
+# import os
+# import urllib.request as request
+# from pathlib import Path
+# from ASLD_step2.utils import get_size
 from zipfile import ZipFile
 import pandas as pd
 import os
@@ -20,59 +20,56 @@ import shutil
 from sklearn.model_selection import train_test_split
 
 class DataIngestion:
-    def __init__(self, config: DataIngestionConfig):
-        self.config = config
+    # def __init__(self, config: DataIngestionConfig):
+    #     self.config = config
 
-    def download_file(self):
-    #this function will download the file and store in local_data_file
-        try:
-            logging.info('Trying to download file')
-            #urlretrieve needs url and path to store file
-            logging.info("Trying to download file...")
+    # def download_file(self):
+    # #this function will download the file and store in local_data_file
+    #     try:
+    #         logging.info('Trying to download file')
+    #         #urlretrieve needs url and path to store file
+    #         logging.info("Trying to download file...")
 
-            if not os.path.exists(self.config.local_data_file):
+    #         if not os.path.exists(self.config.local_data_file):
 
-                logging.info("Download started...")
-                filename, headers = request.urlretrieve(
-                    url=self.config.source_URL,
-                    filename=self.config.local_data_file
-                )
-                logging.info(f"{filename} download! with following info: \n{headers}")
-            else:
-                 logging.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")  
+    #             logging.info("Download started...")
+    #             filename, headers = request.urlretrieve(
+    #                 url=self.config.source_URL,
+    #                 filename=self.config.local_data_file
+    #             )
+    #             logging.info(f"{filename} download! with following info: \n{headers}")
+    #         else:
+    #              logging.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")  
 
-        except Exception as e:
-            raise ASLDException(e,sys) from e
+    #     except Exception as e:
+    #         raise ASLDException(e,sys) from e
 
-      
-
-
-    def check_and_extract_videos(self):
-        """
-        Check if the artifact/videos folder is empty. If empty, unzip video data from WLASL_videos.zip
-        and move only video files into artifact/videos. Then delete the unzip folder. If not empty, return.
-        """
-        try:
+    # def check_and_extract_videos(self):
+    #     """
+    #     Check if the artifact/videos folder is empty. If empty, unzip video data from WLASL_videos.zip
+    #     and move only video files into artifact/videos. Then delete the unzip folder. If not empty, return.
+    #     """
+    #     try:
     
-            if os.path.exists(self.config.unzip_dir):
-                # Unzip video data from WLASL_videos.zip
-                with ZipFile(self.config.local_data_file, 'r') as zip_ref:
-                    zip_ref.extractall(self.config.unzip_dir)
-                logging.info("extraction completed")
-            else:
-                logging.info("error in Extraction...")
-        except Exception as e:
-            raise ASLDException(e,sys) from e
+    #         if os.path.exists(self.config.unzip_dir):
+    #             # Unzip video data from WLASL_videos.zip
+    #             with ZipFile(self.config.local_data_file, 'r') as zip_ref:
+    #                 zip_ref.extractall(self.config.unzip_dir)
+    #             logging.info("extraction completed")
+    #         else:
+    #             logging.info("error in Extraction...")
+    #     except Exception as e:
+    #         raise ASLDException(e,sys) from e
 
-
-
-
-    def split_videos(self):
+    def split_videos():
         # Load the CSV file
-        csv_file = os.path.join(self.config.unzip_dir,'videos_id.csv')
+        csv_file = r"C:\secsem\step\tv_data\WLASL_videos\videos_id.csv"
+        video_folder = "C:/secsem/step/tv_data/WLASL_videos"
+        train_ratio = 0.8
+        # csv_file = os.path.join(self.config.unzip_dir,'videos_id.csv')
         df = pd.read_csv(csv_file)
-        captions = df['caption'].tolist()  # Get the captions column
-        ids = df['ID'].tolist()  # Get the IDs column
+        captions = df['gloss'].tolist()  # Get the captions column
+        ids = df['id'].tolist()  # Get the IDs column
 
         # Create a dictionary mapping IDs to captions
         id_to_caption = dict(zip(ids, captions))
@@ -81,7 +78,7 @@ class DataIngestion:
         video_files = [f for f in os.listdir(video_folder) if f.endswith(".mp4")]
 
         # Split the videos
-        videos_train, videos_test = train_test_split(video_files, train_size=train_ratio, random_state=42)
+        videos_train, videos_test = train_test_split(video_files, train_size=0.2, random_state=42)
 
         # Create train and test folders if they don't exist
         train_folder = "train"
@@ -91,26 +88,23 @@ class DataIngestion:
 
         # Move videos to train folder and label them
         for video in videos_train:
-            video_id = os.path.splitext(video)[0]
+            video_id = int(os.path.splitext(video)[0])
             caption = id_to_caption.get(video_id, "Unknown")  # Get the caption for the video ID
-            labeled_video = f"{caption}_{video}"
+            labeled_video = f"{caption}.mp4"
             shutil.move(os.path.join(video_folder, video), os.path.join(train_folder, labeled_video))
 
         # Move videos to test folder and label them
         for video in videos_test:
-            video_id = os.path.splitext(video)[0]
+            video_id = int(os.path.splitext(video)[0])
             caption = id_to_caption.get(video_id, "Unknown")  # Get the caption for the video ID
-            labeled_video = f"{caption}_{video}"
-            shutil.move(os.path.join(video_folder, video), os.path.join(test_folder, labeled_video))
+            labeled_video = f"{caption}.mp4"
+            shutil.move(os.path.join("C:\secsem\step\tv_data\demo_videos", video), os.path.join(test_folder, labeled_video))
 
         print("Videos split into train and test sets.")
 
 
     # Example usage
-    csv_file = "captions.csv"
-    video_folder = "videos"
-    train_ratio = 0.8
 
-    split_videos(csv_file, video_folder, train_ratio)
+    split_videos()
 
 
